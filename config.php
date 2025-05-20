6392f4df9e80711c2a695513519ea969184531d2
0x1git <giteshbharambe33@gmail.com>
2025-05-20 14:03:27 +0530

    update database name in configuration

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $username = "root";
 $password = "228899";
-$database = "goldenpalacehotel";
+$database = "bluebirdhotel";
 
 $conn = mysqli_connect($server,$username,$password,$database);
 
